ae83bdcb9f3b415926df138bc4b97fd2220b9887
gminova <minova1995@gmail.com>
2019-04-22 20:15:40 +0100

    refactored code

--- script.js
+++ script.js
@@ -6,83 +6,13 @@ var negate = document.getElementById("negate");
 var stored = undefined;
 var lastOp = undefined;
 
-var one = document.getElementById("one");
-one.onclick = function () {
-    txt.value += "1";
-    if (stored == undefined) {
-        result.value = "";
-    }
-}
-
-var two = document.getElementById("two");
-two.onclick = function () {
-    txt.value += "2";
-    if (stored == undefined) {
-        result.value = "";
-    }
-}
-
-var three = document.getElementById("three");
-three.onclick = function () {
-    txt.value += "3";
-    if (stored == undefined) {
-        result.value = "";
-    }
-}
-
-var four = document.getElementById("four");
-four.onclick = function () {
-    txt.value += "4";
-    if (stored == undefined) {
-        result.value = "";
-    }
-}
-
-var five = document.getElementById("five");
-five.onclick = function () {
-    txt.value += "5";
-    if (stored == undefined) {
-        result.value = "";
-    }
-}
-
-var six = document.getElementById("six");
-six.onclick = function () {
-    txt.value += "6";
-    if (stored == undefined) {
-        result.value = "";
-    }
-}
-
-var seven = document.getElementById("seven");
-seven.onclick = function () {
-    txt.value += "7";
-    if (stored == undefined) {
-        result.value = "";
-    }
-}
-
-var eight = document.getElementById("eight");
-eight.onclick = function () {
-    txt.value += "8";
-    if (stored == undefined) {
-        result.value = "";
-    }
-}
-
-var nine = document.getElementById("nine");
-nine.onclick = function () {
-    txt.value += "9";
-    if (stored == undefined) {
-        result.value = "";
-    }
-}
-
-var zero = document.getElementById("zero");
-zero.onclick = function () {
-    txt.value += "0";
-    if (stored == undefined) {
-        result.value = "";
+for (let id = 0; id <= 9; id++){
+    var num = document.getElementById(id.toString());
+    num.onclick = function () {
+        txt.value += id.toString();
+        if (stored == undefined) {
+                result.value = "";
+        }
     }
 }
 
@@ -105,57 +35,21 @@ var apply = function(value) {
 
 };
 
-var plus = document.getElementById("plus");
-plus.onclick = function () {
-    if (stored == undefined) {
-        stored = Number(txt.value);
-        result.value = stored.toString(); 
-    } else {
-        apply(Number(txt.value));
+var operations = ["plus", "minus", "multiply", "divide"];
+
+operations.forEach(op => {
+    var element = document.getElementById(op);
+    element.onclick = function () {
+        if (stored == undefined) {
+            stored = Number(txt.value);
+            result.value = stored.toString(); 
+        } else {
+            apply(Number(txt.value));
+        }
+        txt.value = "";
+        lastOp = op;
     }
-
-    txt.value = "";
-    lastOp = "plus";
-}
-
-var minus = document.getElementById("minus");
-minus.onclick = function () {
-    if (stored == undefined) {
-        stored = Number(txt.value);
-        result.value = stored.toString(); 
-    } else {
-        apply(Number(txt.value));
-    }
-
-    txt.value = "";
-    lastOp = "minus";
-}
-
-var divide = document.getElementById("divide");
-divide.onclick = function () {
-    if (stored == undefined) {
-        stored = Number(txt.value);
-        result.value = stored.toString(); 
-    } else {
-        apply(Number(txt.value));
-    }
-
-    txt.value = "";
-    lastOp = "divide";
-}
-
-var multiply = document.getElementById("multiply");
-multiply.onclick = function () {
-    if (stored == undefined) {
-        stored = Number(txt.value);
-        result.value = stored.toString(); 
-    } else {
-        apply(Number(txt.value));
-    }
-
-    txt.value = "";
-    lastOp = "multiply";
-}
+});
 
 var dot = document.getElementById("dot");
 dot.onclick = function () {
