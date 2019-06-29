7708cba7ca8ecb4df12ff531ca4d1ef30df1ef4c
gminova <minova1995@gmail.com>
2019-06-29 07:16:07 +0100

    multiple zero input fixed

--- script.js
+++ script.js
@@ -6,31 +6,31 @@ let negate = document.getElementById("negate");
 let stored = undefined;
 let lastOp = undefined;
 
-for (let id = 0; id <= 9; id++){
+for (let id = 0; id <= 9; id++) {
     let num = document.getElementById(id.toString());
     num.onclick = function () {
+        if (id == 0 && output.value == '') {
+            num.disabled = true;
+        }
         output.value += id.toString();
         if (stored == undefined) {
-                result.value = "";
+            result.value = "";
         }
     }
 }
 
-let apply = function(value) {
+let apply = function (value) {
     if (value != undefined) {
         if (lastOp == "add") {
-            stored = parseFloat(stored+value);
+            stored = parseFloat(stored + value);
+        } else if (lastOp == "subtract") {
+            stored = parseFloat(stored - value);
+        } else if (lastOp == "multiply") {
+            stored = parseFloat(stored * value);
+        } else if (lastOp == "divide") {
+            stored = parseFloat(stored / value);
         }
-        else if (lastOp == "subtract") {
-            stored = parseFloat(stored-value);
-        }
-        else if (lastOp == "multiply") {
-            stored = parseFloat(stored*value);
-        }
-        else if (lastOp == "divide") {
-            stored = parseFloat(stored/value);
-        }
-        result.value = stored.toString(); 
+        result.value = stored.toString();
     }
 
 };
@@ -42,7 +42,7 @@ operations.forEach(op => {
     element.onclick = function () {
         if (stored == undefined) {
             stored = Number(output.value);
-            result.value = stored.toString(); 
+            result.value = stored.toString();
         } else {
             apply(Number(output.value));
         }
@@ -55,12 +55,14 @@ let decimal = document.getElementById("decimal");
 decimal.onclick = function () {
     if (output.value.indexOf(decimal.value) == -1) {
         output.value += decimal.value;
+        let zero = document.getElementById('0');
+        zero.disabled = false;
     }
 }
 
 let equals = document.getElementById("equals");
 equals.onclick = function () {
-    apply(Number(output.value)); 
+    apply(Number(output.value));
     output.value = "";
     // stored = undefined;
     lastOp = undefined;
